cfe57dadff45eb312be7424c82bbd898b24d6f14
Kai Sickeler <k.sickeler@gmail.com>
2017-07-31 19:56:57 +0200

    Initial version

---
+++ src/error.rs
@@ -0,0 +1,8 @@
+quick_error!{
+    #[derive(Debug)]
+    pub enum DedupError {
+        FileNotFound(err: ::std::io::Error) { from() }
+        HashError(err: ::filehash::error::FilehashError) { from() }
+        test {}
+    } 
+}

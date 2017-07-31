ce4aa80e2dcdb0e1da86b15a85dc9790472ed2ec
Kai Sickeler <k.sickeler@gmail.com>
2017-07-31 18:49:30 +0200

    Add clap and -p flag

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
 extern crate clap;
 extern crate filehash;
 
+use clap::{Arg, App};
 
 fn main() {
-    println!("debup");
+    let matches = App::new("Dedup")
+        .version("0.1.0")
+        .author("asui <k.sickeler@gmail.com>")
+        .about("Find duplicated files")
+        .arg(Arg::with_name("path")
+             .short("p")
+             .long("path")
+             .value_name("path")
+             .help("Search for dupliates within the given path")
+             .required(true)
+             .takes_value(true))
+        .get_matches();
 }
